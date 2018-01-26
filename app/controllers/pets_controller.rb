7d87b704b3c60b2f1ee2109b4ef8977becdc419c
lindsayka9 <kat.lindsay.92@gmail.com>
2018-01-26 11:05:10 -0500

    Add update curl script - pets
    Add successful update-pets curl script

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -38,7 +38,7 @@ class PetsController < ProtectedController
     @pet.destroy
   end
 
-  # private
+  private
     # Use callbacks to share common setup or constraints between actions.
     def set_pet
       @pet = Pet.find(params[:id])
