bfc15534d514c1e0c0c3ffd0c4f111558df89181
lindsayka9 <kat.lindsay.92@gmail.com>
2018-02-08 15:26:04 -0500

    Fix delete
    Delete feature working
    Show feature working
    
    Redefined path to retrieve `:owner_id` to view and destroy a `pet`
    record.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class PetsController < ProtectedController
-  before_action :set_pet, only: %i[show update destroy]
+  before_action :set_pet, only: %i[update]
+  before_action :set_owner, only: %i[show destroy]
 
   # GET /pets
   def index
@@ -12,6 +13,7 @@ class PetsController < ProtectedController
 
   # GET /pets/1
   def show
+    @pet = current_user.owners.find(@owner).pets.find(params[:id])
     render json: @pet
   end
 
@@ -29,6 +31,8 @@ class PetsController < ProtectedController
 
   # PATCH/PUT /pets/1
   def update
+    binding.pry
+    @pet.owner_id =
     if @pet.update(pet_params)
       render json: @pet
     else
@@ -38,14 +42,20 @@ class PetsController < ProtectedController
 
   # DELETE /pets/1
   def destroy
+    @pet = current_user.owners.find(@owner).pets.find(params[:id])
     @pet.destroy
     # head :no_content
   end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_pet
-    # binding.pry
     @pet = current_user.owners.find(pet_params[:owner_id]).pets.find(params[:id])
+  end
+
+  def set_owner
+    # binding.pry
+    # @owner = current_user.owners.find(params[:owner_id])
+    @owner = current_user.pets.find(params[:id])[:owner_id]
   end
 
   # Only allow a trusted parameter "white list" through.
